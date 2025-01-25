ce1a0272dd7c84f8d501c1c9f010689345da7af7
종달새\82103 <kcood92@gmail.com>
2025-01-25 15:59:11 +0900

    first commit

---
+++ src/main/java/com/lark/firstProject/ZodiacTellerApplication.java
@@ -0,0 +1,13 @@
+package com.lark.firstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZodiacTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZodiacTellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lark/firstProject/ZodiacTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lark.firstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacTellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
